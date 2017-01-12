1e7f616ea3d68c57942fa4d34e552c7f85b4e49a
Thomas Lock <email@peb.si>
2017-01-12 13:53:19 +0000

    add ship customisation

--- js/order.js
+++ js/order.js
@@ -67,3 +67,93 @@ var trailText = [
   "Orange trails",
   "Blue trails"
 ];
+
+var bodyIndex, utilIndex, scanIndex, trailIndex;
+
+bodyIndex = utilIndex = scanIndex = trailIndex = 0; //set to 0 initially
+
+var bodyImage, utilImage, scanImage, trailImage;
+
+window.onload = function()
+{
+  bodyImage = document.getElementById("bodyImg");
+  utilImage = document.getElementById("utilImg");
+  scanImage = document.getElementById("scanImg");
+  trailImage = document.getElementById("trailImg");
+
+  onShipChanged();
+}
+
+function onBodyChange(offset)
+{
+  var offsetIndex = (bodyIndex + offset);
+
+  if (offsetIndex < 0) {
+    bodyIndex = bodyImages.length + offset;
+  } else {
+    bodyIndex = (bodyIndex + offset) % bodyImages.length;
+  }
+
+  onShipChanged();
+}
+
+function onUtilChange(offset)
+{
+  var offsetIndex = (utilIndex + offset);
+
+  if (offsetIndex < 0) {
+    utilIndex = utilImages.length + offset;
+  } else {
+    utilIndex = (utilIndex + offset) % utilImages.length;
+  }
+
+  onShipChanged();
+}
+
+function onScanChange(offset)
+{
+  var offsetIndex = (scanIndex + offset);
+
+  if (offsetIndex < 0) {
+    scanIndex = scanImages.length + offset;
+  } else {
+    scanIndex = (scanIndex + offset) % scanImages.length;
+  }
+
+  onShipChanged();
+}
+
+function onTrailChange(offset)
+{
+  var offsetIndex = (trailIndex + offset);
+
+  if (offsetIndex < 0) {
+    trailIndex = trailImages.length + offset;
+  } else {
+    trailIndex = (trailIndex + offset) % trailImages.length;
+  }
+
+  onShipChanged();
+}
+
+//update images
+function onShipChanged() {
+  bodyImage.src = bodyImages[bodyIndex];
+  utilImage.src = utilImages[utilIndex];
+  scanImage.src = scanImages[scanIndex];
+  trailImage.src = trailImages[trailIndex];
+}
+
+function saveSelection() {
+  localStorage.setItem("chosenBody", bodyIndex);
+  localStorage.setItem("chosenUtil", utilIndex);
+  localStorage.setItem("chosenScan", scanIndex);
+  localStorage.setItem("chosenTrail", trailIndex);
+}
+
+function loadSelection() {
+  bodyIndex = localStorage.getItem("chosenBody");
+  utilIndex = localStorage.getItem("chosenUtil");
+  scanIndex = localStorage.getItem("chosenScan");
+  trailIndex = localStorage.getItem("chosenTrail");
+}
