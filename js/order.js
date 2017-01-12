fcf704a25d33b9b6c0dcaf075e5141d72a1d8e4d
Thomas Lock <email@peb.si>
2017-01-12 12:54:46 +0000

    add order.js

---
+++ js/order.js
@@ -0,0 +1,69 @@
+//Body images
+var bodyImages = [
+  "img/ship/cruise.png",
+  "img/ship/cargo.png",
+  "img/ship/viper.png"
+];
+
+var bodyCost = [
+  1000,
+  2500,
+  7500
+];
+
+var bodyText = [
+  "Cruiser",
+  "Cargo Hauler",
+  "Viper Sports Ship"
+];
+
+//Utility images
+var utilImages = [
+  "img/ship/miner.png",
+  "img/ship/laser.png"
+];
+
+var utilCost = [
+  200,
+  400
+];
+
+var utilText = [
+  "Mining laser",
+  "Civilian defense laser"
+];
+
+//Scanner images
+var scanImages = [
+  "img/ship/scan1.png",
+  "img/ship/scan2.png",
+  "img/ship/scan3.png"
+];
+
+var scanCost = [
+  100,
+  200,
+  400
+];
+
+var scanText = [
+  "100km^2 scanner",
+  "250km^2 scanner",
+  "1000km^2 scanner"
+];
+
+//Trail images
+var trailImages = [
+  "img/ship/orange.png",
+  "img/ship/blue.png"
+];
+
+var trailCost = [
+  0,
+  0
+];
+
+var trailText = [
+  "Orange trails",
+  "Blue trails"
+];
